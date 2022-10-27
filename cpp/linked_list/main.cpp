cc38f6b5f12c52754fe0245468e6885f4d6447a3
Eray Gökgör <eray.gokgor@apps.com.tr>
2022-10-27 23:09:25 +0300

    add new functions

--- cpp/linked_list/main.cpp
+++ cpp/linked_list/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 
-using namespace std;
-
 // Node
 template <class T>
 class Node {
@@ -33,7 +31,13 @@ public:
 
     void insert(const T& data, Node<T>* p);
     Node<T>* find(const T& data);
-    Node<T*> findPrevious(const T& data);
+    Node<T>* findPrevious(const T& data);
+    void remove(const T& data);
+    void print() const;
+    void makeEmpty();
+    ~List();
+    List& operator=(const List& rhs);
+    List(const List& rhs);
 };
 
 
@@ -59,7 +63,7 @@ Node<T>* List<T>::find(const T& data) {
 
 // previous finder
 template <class T>
-Node<T*> List<T>::findPrevious(const T& data) {
+Node<T>* List<T>::findPrevious(const T& data) {
     Node<T>* p = zeroth();
     while (p->next) {
         if (p->next->element==data) {
@@ -70,9 +74,127 @@ Node<T*> List<T>::findPrevious(const T& data) {
     return NULL;
 }
 
+// remove element found first
+template <class T>
+void List<T>::remove(const T &data) {
+    Node<T>* p = findPrevious(data);
+
+    if (p) {
+        Node<T>* tmp = p->next;
+        p->next = tmp->next;
+        delete tmp;
+    }
+}
+
+// print all elements
+template <class T>
+void List<T>::print() const {
+    const Node<T>* p = first();
+
+    while (p){
+        std::cout<<p->element<<std::endl;
+        p = p->next;
+    }
+}
+
+// empty list
+template <class T>
+void List<T>::makeEmpty() {
+    while (!isEmpty()){
+        remove(first()->element);
+    }
+}
+
+template<class T>
+List<T>::~List() {
+    makeEmpty();
+
+    delete dummyHead;
+}
+
+// assignment operator
+template <class T>
+List<T>& List<T>::operator=(const List& rhs) {
+    if (this != &rhs) {
+        makeEmpty();
+        const Node<T>* r = rhs.first();
+        Node<T>* p = zeroth();
+
+        while (r) {
+            insert(r->element, p);
+            r = r->next;
+            p = p->next;
+        }
+    }
+    return *this;
+}
+
+// copy constructor
+template <class T>
+List<T>::List(const List& rhs) {
+    dummyHead = new Node<T>(T(), NULL);
+
+    *this = rhs; // use operator
+    }
+
 
 
 int main() {
+
+    // Testing
+
+    // Create
+    List<int> list;
+    list.insert(0, list.zeroth());
+    Node<int>* p = list.first();
+
+    // Insertion
+    for (int i = 1; i <= 10; ++i)
+    {
+        list.insert(i, p);
+        p = p->next;
+    }
+    // Print
+    std::cout << "printing original list" << std::endl;
+    list.print();
+
+    // Remove
+    for (int i = 0; i <= 10; ++i)
+    {
+        if (i % 2 == 0)
+            list.remove(i);
+    }
+
+    std::cout << "printing odd number list" << std::endl;
+    list.print();
+
+    // Assignment
+    List<int> list2 = list;
+    std::cout << "printing copy constructed list" << std::endl;
+    list2.print();
+
+    List<int> list3;
+    list3 = list;
+    std::cout << "printing assigned list" << std::endl;
+    list3.print();
+
+    // Empty
+    list.makeEmpty();
+    std::cout << "printing emptied list" << std::endl;
+    list.print();
+
+    // Find
+    for (int i = 0; i <= 10; ++i) {
+        if (i % 2 == 0) {
+            if (list2.find(i) == NULL)
+                std::cout << "could not find element " << i << std::endl;
+        }
+        else {
+            if (list2.find(i) != NULL)
+                std::cout << "found element " << i << std::endl;
+        }
+    }
+
 
     return 0;
 }
