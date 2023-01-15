40d62b7970095a8ea9cfc4247eec7a58084d8827
Eray Gökgör <gokgoreray@gmail.com>
2023-01-15 17:01:16 +0300

    add some stacks

--- cpp/Stack/Stack/Stack.cpp
+++ cpp/Stack/Stack/Stack.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include "Stack.h"
 
 int main() {
-    std::cout << "Hello, World!\n";
+    // Some tests for stack adt
+
+    Stack<int> s;
+    for (int i = 0; i < 10; i++)
+        s.push(i);
+    Stack<int> s2 = s;
+    std::cout << "Printing s:" << std::endl;
+    while (!s.isEmpty()) {
+        int value;
+        s.topAndPop(value);
+        std::cout << value << std::endl;
+    }
+    
+    std::cout << "Printing s2:" << std::endl;
+    while (!s2.isEmpty()) {
+        int value;
+        s2.topAndPop(value);
+        std::cout << value << std::endl;
+    }
+    
+    
+    
     return 0;
 }
