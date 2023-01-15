60a266f27511c011a3a99159894543493cba4ad3
Eray Gökgör <gokgoreray@gmail.com>
2023-01-15 14:16:17 +0300

    update files

--- cpp/LinkedList/LinkedList/LinkedList.h
+++ cpp/LinkedList/LinkedList/LinkedList.h
@@ -42,4 +42,103 @@ public:
     List(const List& rhs);
 };
 
+
+// element inserter
+template <class T>
+void List<T>::insert(const T &data, Node<T> *p) {
+    Node<T>* newNode = new Node<T>(data, p->next);
+    p->next = newNode;
+}
+
+// element finder
+template <class T>
+Node<T>* List<T>::find(const T& data) {
+    Node<T>* p = first();
+    while (p) {
+        if (p->element == data){
+            return p;
+        }
+        p = p->next;
+    }
+    return NULL;
+}
+
+// previous finder
+template <class T>
+Node<T>* List<T>::findPrevious(const T& data) {
+    Node<T>* p = zeroth();
+    while (p->next) {
+        if (p->next->element==data) {
+            return p;
+        }
+        p = p->next;
+    }
+    return NULL;
+}
+
+// remove element found first
+template <class T>
+void List<T>::remove(const T &data) {
+    Node<T>* p = findPrevious(data);
+
+    if (p) {
+        Node<T>* tmp = p->next;
+        p->next = tmp->next;
+        delete tmp;
+    }
+}
+
+// print all elements
+template <class T>
+void List<T>::print() const {
+    const Node<T>* p = first();
+
+    while (p){
+        std::cout<<p->element<<std::endl;
+        p = p->next;
+    }
+}
+
+// empty list
+template <class T>
+void List<T>::makeEmpty() {
+    while (!isEmpty()){
+        remove(first()->element);
+    }
+}
+
+template<class T>
+List<T>::~List() {
+    makeEmpty();
+
+    delete dummyHead;
+}
+
+// assignment operator
+template <class T>
+List<T>& List<T>::operator=(const List& rhs) {
+    if (this != &rhs) {
+        makeEmpty();
+        const Node<T>* r = rhs.first();
+        Node<T>* p = zeroth();
+
+        while (r) {
+            insert(r->element, p);
+            r = r->next;
+            p = p->next;
+        }
+    }
+    return *this;
+}
+
+// copy constructor
+template <class T>
+List<T>::List(const List& rhs) {
+    dummyHead = new Node<T>(T(), NULL);
+
+    *this = rhs; // use operator
+}
+
+
+
 #endif //LINKED_LIST_LINKEDLIST_H
