684199f46eac32ce4983516427395e5042a563ac
Eray Gökgör <gokgoreray@gmail.com>
2023-01-15 17:09:01 +0300

    initial commit

---
+++ cpp/Queue/Queue/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Queue
+//
+//  Created by Eray Gökgör on 15.01.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
